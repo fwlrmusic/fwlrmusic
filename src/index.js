e0fdd3aa38a76767ffe50f3d21f062ec77bcf584
jordanmceachern <jormceachern@gmail.com>
2020-08-23 19:09:10 -0300

    markup and component structure

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './App.css'
+import './resources/App.css'
 import Navbar from './navbar'
+import Landing from './landing'
 
 function App () {
   return (
     <div className='App'>
       <Navbar />
       <div className='page-content'>
-        <div className='landing-container'>
-          <h1>Landing Page</h1>
-          <div className='landing-image'>
-            <img
-              src='https://res.cloudinary.com/dtweazqf2/image/upload/v1598215517/fwlr_landing_m8osmn.png'
-              alt='Producer Nick Fowler poses atop his Talon car'
-            />
-          </div>
-        </div>
+        {/* React Router */}
+        <Landing />
       </div>
     </div>
   )
---
+++ src/landing.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './resources/Landing.css'
+
+const Landing = () => {
+  return (
+    <div className='landing-container'>
+      <h1>Landing Page</h1>
+      <div className='landing-image'>
+        <img
+          src='https://res.cloudinary.com/dtweazqf2/image/upload/v1598215517/fwlr_landing_m8osmn.png'
+          alt='Producer Nick Fowler poses atop his Talon car'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Landing
