3e9e1bdb2196ae03accaa41201c28ec68154824f
jordanmceachern <jormceachern@gmail.com>
2020-08-24 11:44:52 -0300

    navigation complete

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './resources/App.css'
-import Navbar from './navbar'
-import Landing from './landing'
+import Navbar from './Navbar'
+import GrandPrix from './GrandPrix'
+import PitCrew from './PitCrew'
+import Contact from './Contact'
+import Landing from './Landing'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 function App () {
   return (
-    <div className='App'>
-      <Navbar />
-      <div className='page-content'>
-        {/* React Router */}
-        <Landing />
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='page-content'>
+          <Switch>
+            <Route path='/grand-prix'>
+              <GrandPrix />
+            </Route>
+            <Route path='/pit-crew'>
+              <PitCrew />
+            </Route>
+            <Route path='/contact'>
+              <Contact />
+            </Route>
+            <Route path='/'>
+              <Landing />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   )
 }
 
--- src/navbar.js
+++ src/navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './resources/Navbar.css'
 import { CgMenuRightAlt } from 'react-icons/cg'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -9,34 +10,40 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen)
   }
 
+  const closeMenu = () => {
+    setIsMenuOpen(false)
+  }
+
   const menuItems = (
     <ul>
-      <li>
-        <a href='#'>
+      <li onClick={toggleMenu}>
+        <Link to='/' title='Home page'>
             Home
-        </a>
+        </Link>
       </li>
-      <li>
-        <a href='#'>
+      <li onClick={toggleMenu}>
+        <Link to='/grand-prix' title='Grand Prix'>
             FWLR Grand Prix
-        </a>
+        </Link>
       </li>
-      <li>
-        <a href='#'>
+      <li onClick={toggleMenu}>
+        <Link to='/pit-crew' title='Pit Crew'>
             Pit Crew
-        </a>
+        </Link>
       </li>
-      <li>
-        <a href='#'>
+      <li onClick={toggleMenu}>
+        <Link to='/contact' title='Contact'>
             Contact
-        </a>
+        </Link>
       </li>
     </ul>
   )
   const burgerMenu = () => {
     return (
       <div className='burger-menu'>
-        <img src='https://res.cloudinary.com/dtweazqf2/image/upload/h_40,f_auto,q_auto/v1598272317/fwlr_logo_e7lfmo.png' alt='Fowler logo' />
+        <Link to='/' onClick={closeMenu}>
+          <img src='https://res.cloudinary.com/dtweazqf2/image/upload/h_40,f_auto,q_auto/v1598272317/fwlr_logo_e7lfmo.png' alt='Fowler logo' onClick={toggleMenu} />
+        </Link>
         {
           isMenuOpen && (
             menuItems
