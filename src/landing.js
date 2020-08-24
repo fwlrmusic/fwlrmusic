74cfda0e916e2f41519a8f5e816edd1814c18ebe
jordanmceachern <jormceachern@gmail.com>
2020-08-24 11:52:37 -0300

    Update Landing.js name

--- src/landing.js
+++ src/landing.js
@@ -5,6 +5,7 @@ const Landing = () => {
   return (
     <div className='landing-container'>
       <div className='landing-image'>
+        {/* TODO: Update image once higher resolution is obtained */}
         <img
           src='https://res.cloudinary.com/dtweazqf2/image/upload/h_400,f_auto,q_auto/v1598215517/fwlr_landing_m8osmn.png'
           alt='Producer Nick Fowler poses atop his Talon car'
