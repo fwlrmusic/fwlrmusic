93c6a9ee76a490a97a221ec48304ffe16ec44289
jordanmceachern <jormceachern@gmail.com>
2020-08-25 15:04:22 -0300

    landing cloudinary url edit

--- src/Landing.js
+++ src/Landing.js
@@ -6,7 +6,7 @@ const Landing = () => {
     <div className='landing-container'>
       <div className='landing-image'>
         <img
-          src='https://res.cloudinary.com/dtweazqf2/image/upload/f_auto,q_auto,c_fit/v1598215517/fwlr_landing_m8osmn.png'
+          src='https://res.cloudinary.com/dtweazqf2/image/upload/h_400,w_auto,f_auto,q_auto,c_fill/v1598215517/fwlr_landing_m8osmn.png'
           alt='Producer Nick Fowler poses atop his Talon car'
         />
       </div>
