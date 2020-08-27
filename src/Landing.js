5e6976167ba275383b19f9435467f6d833afc763
jordanmceachern <jormceachern@gmail.com>
2020-08-27 12:02:30 -0300

    improve landing image sharpness

--- src/Landing.js
+++ src/Landing.js
@@ -8,12 +8,12 @@ const Landing = () => {
     <div className='landing-container'>
       <div className='landing-image'>
         <img
-          src='https://res.cloudinary.com/dtweazqf2/image/upload/h_400,w_auto,f_auto,q_auto,c_fill/v1598539247/Original_FWLR_Landing_obgvba.jpg'
+          src='https://res.cloudinary.com/dtweazqf2/image/upload/h_4000,w_auto,f_auto,q_auto,g_auto,e_sharpen,c_fill/v1598539247/Original_FWLR_Landing_obgvba.jpg'
           alt='Producer Nick Fowler poses atop his Talon car'
         />
       </div>
       <div className='landing-logo'>
-        <img src='https://res.cloudinary.com/dtweazqf2/image/upload/h_100,f_auto,q_auto,c_fit/v1598539128/Original_FWLR_Logo_htth7t.png' alt='Fowler logo' />
+        <img src='https://res.cloudinary.com/dtweazqf2/image/upload/h_220,f_auto,q_auto,c_fit/v1598539128/Original_FWLR_Logo_htth7t.png' alt='Fowler logo' />
       </div>
       <div className='social-links-container'>
         <a
