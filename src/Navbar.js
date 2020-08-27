8ab23ec912267fd2a36bf6e007c173c560526631
jordanmceachern <jormceachern@gmail.com>
2020-08-27 10:02:39 -0300

    optimize Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './resources/Navbar.css'
 import { CgMenuRightAlt } from 'react-icons/cg'
 import { Link } from 'react-router-dom'
@@ -6,33 +6,46 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      const menuItems = Array.from(document.getElementsByClassName('menu-item'))
+      if (menuItems) {
+        menuItems.forEach(menuItem => {
+          menuItem.addEventListener('click', closeMenu)
+        })
+      }
+    }
+  })
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
   }
 
   const closeMenu = () => {
-    setIsMenuOpen(false)
+    if (isMenuOpen) {
+      setIsMenuOpen(false)
+    }
   }
 
   const menuItems = (
     <ul>
-      <li onClick={toggleMenu}>
-        <Link to='/' title='Home page'>
+      <li>
+        <Link to='/' title='Home page' className='menu-item'>
             Home
         </Link>
       </li>
-      <li onClick={toggleMenu}>
-        <Link to='/grand-prix' title='Grand Prix'>
+      <li>
+        <Link to='/grand-prix' title='Grand Prix' className='menu-item'>
             FWLR Grand Prix
         </Link>
       </li>
-      <li onClick={toggleMenu}>
-        <Link to='/pit-crew' title='Pit Crew'>
+      <li>
+        <Link to='/pit-crew' title='Pit Crew' className='menu-item'>
             Pit Crew
         </Link>
       </li>
-      <li onClick={toggleMenu}>
-        <Link to='/contact' title='Contact'>
+      <li>
+        <Link to='/contact' title='Contact' className='menu-item'>
             Contact
         </Link>
       </li>
@@ -42,7 +55,11 @@ const Navbar = () => {
     return (
       <div className='burger-menu'>
         <Link to='/' onClick={closeMenu}>
-          <img src='https://res.cloudinary.com/dtweazqf2/image/upload/h_40,f_auto,q_auto/v1598272317/fwlr_logo_e7lfmo.png' alt='Fowler logo' onClick={toggleMenu} />
+          <img
+            src='https://res.cloudinary.com/dtweazqf2/image/upload/h_40,f_auto,q_auto/v1598272317/fwlr_logo_e7lfmo.png'
+            alt='Fowler logo'
+            onClick={closeMenu}
+          />
         </Link>
         {
           isMenuOpen && (
