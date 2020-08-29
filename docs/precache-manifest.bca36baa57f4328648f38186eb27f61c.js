d5008c29c03fccb3b5851240aba6cf5a727f017a
jordanmceachern <jormceachern@gmail.com>
2020-08-29 09:19:08 -0300

    deploy latest changes

---
+++ docs/precache-manifest.bca36baa57f4328648f38186eb27f61c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f860811b517918feb9f618f24143c6c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7be233394b32e34a52c2",
+    "url": "/static/css/4.380d2927.chunk.css"
+  },
+  {
+    "revision": "87016c93f7e3cf00fc93",
+    "url": "/static/css/5.4f94379d.chunk.css"
+  },
+  {
+    "revision": "5ee97fb165dd462fbbf4",
+    "url": "/static/css/6.095c0b16.chunk.css"
+  },
+  {
+    "revision": "1e421814ccd923904dee",
+    "url": "/static/css/7.e8d06e7e.chunk.css"
+  },
+  {
+    "revision": "4246f3c1aaed66583667",
+    "url": "/static/css/main.d1693b55.chunk.css"
+  },
+  {
+    "revision": "2421dc943493b7d130d1",
+    "url": "/static/js/0.86703b32.chunk.js"
+  },
+  {
+    "revision": "423943e325f5ba57b6d7",
+    "url": "/static/js/3.c3821251.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.c3821251.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be233394b32e34a52c2",
+    "url": "/static/js/4.5d07f0f8.chunk.js"
+  },
+  {
+    "revision": "87016c93f7e3cf00fc93",
+    "url": "/static/js/5.fa99a8aa.chunk.js"
+  },
+  {
+    "revision": "5ee97fb165dd462fbbf4",
+    "url": "/static/js/6.80a80a0c.chunk.js"
+  },
+  {
+    "revision": "1e421814ccd923904dee",
+    "url": "/static/js/7.ec1efb9a.chunk.js"
+  },
+  {
+    "revision": "4246f3c1aaed66583667",
+    "url": "/static/js/main.d97c8c1e.chunk.js"
+  },
+  {
+    "revision": "668d1114aebafa08d511",
+    "url": "/static/js/runtime-main.1b2b6197.js"
+  },
+  {
+    "revision": "add0c4244a015960586eeb51f091fd71",
+    "url": "/static/media/Orbitron-Regular.add0c424.ttf"
+  }
+]);
