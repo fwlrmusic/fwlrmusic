daee832d0f82abbc111d8c22466dff782ae376ff
jordanmceachern <jormceachern@gmail.com>
2020-08-30 01:23:23 -0300

    progress implementing gmail api

--- src/Contact.js
+++ src/Contact.js
@@ -8,6 +8,7 @@ const Contact = () => {
   const [personEmail, setEmail] = useState('')
   const [personMessage, setMessage] = useState('')
   const [inputError, setInputError] = useState(false)
+  const [isDisabled, setIsDisabled] = useState(false)
   //
   const updateValue = e => {
     e.preventDefault()
@@ -29,7 +30,33 @@ const Contact = () => {
   const sendEmail = () => {
     const contactForm = formRef.current
     if (contactForm.checkValidity()) {
-      // send email
+      setIsDisabled(true)
+      // Prepare to send email
+      const clientId = '563488244994-i7rg28cv020s1a7frlapbmouij831f3o.apps.googleusercontent.com'
+      const apiKey = 'AIzaSyA3RSBiQoyzHGwHkodM8Ngybb3I5YRvA8w'
+      const scopes = 'https://www.googleapis.com/auth/gmail.send'
+      // const clientSecret: YcW67MoLzR0t9yW9_UfXv-zB
+      //
+      let email = ''
+      const message = personName + '-' + personEmail + ' says:' + personMessage
+      const headersObj = {
+        To: 'fwlrmusic@gmail.com',
+        Subject: 'New Contact Message from your FWLR Music web app'
+      }
+      //
+      for (var header in headersObj) { email += header += ': ' + headersObj[header] + '\r\n' }
+      email += '\r\n' + message
+      //
+      const sendRequest = gapi.client.gmail.users.messages.send({
+        userId: clientId,
+        resource: {
+          raw: window.btoa(email).replace(/\+/g, '-').replace(/\//g, '_')
+        }
+      })
+      // Send contact email, then set isDisabled to false as the specified callback
+      sendRequest.execute(setIsDisabled(false))
+      //
+      // If user previously triggered an invalid input but has now reached this validated location:
       if (inputError) {
         setInputError(false)
       }
@@ -71,6 +98,7 @@ const Contact = () => {
             <input
               type='submit'
               value='Submit'
+              disabled={isDisabled}
             />
           </div>
         </form>
