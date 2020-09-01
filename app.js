b7003ce37f8cb5e2ba7901d9e8a773143ac32e4a
jordanmceachern <jormceachern@gmail.com>
2020-08-31 22:01:45 -0300

    progress point

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.send()
+  res.send('helloooo')
+})
+
+app.get('/database/:search', (req, res) => {
+  res.send('Database connected')
+  // const search = req.params.search
 })
 
 app.listen(port, () => {
   console.log(`FWLR app listening at http://localhost:${port}`)
 })
 
-app.use(express.static('docs'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('docs'))
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'docs', 'index.html'))
+  })
+}
+
+//
+// const MongoClient = require('mongodb').MongoClient
+// const uri = 'mongodb+srv://fwlrGrandPrix:vtYefRZKiHI19kBJ@cluster0.1ok79.mongodb.net/fwlrgrandprix?retryWrites=true&w=majority'
+// const client = new MongoClient(uri, { useNewUrlParser: true })
+// client.connect(err => {
+//   const collection = client.db('test').collection('devices')
+//   console.log('hi')
+//   client.close()
+//   if (err) {
+//     console.error(err)
+//   }
+// })
+
+// try {
+//   await client.connect()
+//   console.log('Connected correctly to server')
+//   const database = client.db('cluster0')
+//   const myCollection = database.collection('fwlrGrandPrix')
+//   const secretAnswer = await myCollection.findOne({ name: userInput }, { _id: 0 })
+//   return secretAnswer[userInput]
+// } catch (err) {
+//   console.error(err.stack)
+// } finally {
+//   await client.close()
+// }
--- frontend/src/CodeValidator.js
+++ frontend/src/CodeValidator.js
@@ -2,10 +2,6 @@ import React, { useState, useRef } from 'react'
 import './resources/CodeValidator.css'
 import SocialLinks from './SocialLinks'
 import LoadingSpinner from './LoadingSpinner'
-//
-const MongoClient = require('mongodb').MongoClient
-// const uri = 'mongodb+srv://fwlrGrandPrix:vtYefRZKiHI19kBJ@cluster0.1ok79.mongodb.net/fwlrgrandprix?retryWrites=true&w=majority'
-// const client = new MongoClient(uri, { useNewUrlParser: true })
 
 const CodeValidator = () => {
   const [isDisabled, setIsDisabled] = useState(false)
@@ -13,53 +9,40 @@ const CodeValidator = () => {
   const inputRef = useRef(null)
   const codeReturnRef = useRef(null)
 
-  const runQuery = userInput => {
-    setTimeout(() => setIsValidating(false), 2000)
-    // client.connect(err => {
-    //   const collection = client.db('test').collection('devices')
-    //   console.log('hi')
-    //   client.close()
-    //   if (err) {
-    //     console.error(err)
-    //   }
-    // })
-
-    // try {
-    //   await client.connect()
-    //   console.log('Connected correctly to server')
-    //   const database = client.db('cluster0')
-    //   const myCollection = database.collection('fwlrGrandPrix')
-    //   const secretAnswer = await myCollection.findOne({ name: userInput }, { _id: 0 })
-    //   return secretAnswer[userInput]
-    // } catch (err) {
-    //   console.error(err.stack)
-    // } finally {
-    //   await client.close()
-    // }
+  const runQuery = async userInput => {
+    const response = await fetch(`/database/:${userInput}`, {
+      method: 'GET',
+      mode: 'cors',
+      cache: 'no-cache',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    return response // response.json()
   }
 
-  const queryDatabase = e => {
+  const queryDatabase = async e => {
     e.preventDefault()
     setIsDisabled(true)
     const input = inputRef.current
     const codeReturn = codeReturnRef.current
-    // let inputValue
-    // let secretAnswer
+    let inputValue
+    let secretAnswer
     if (input && codeReturn) {
       setIsValidating(true)
-      // inputValue = input.value
+      inputValue = input.value
       runQuery()
-      // secretAnswer = runQuery(inputValue).catch(console.dir)
-      // setIsValidating(false)
-      // setIsDisabled(false)
-      // if (secretAnswer) {
-      //   console.log(secretAnswer)
-      //   codeReturn.textContent = 'Validated! Secret answer is: ' + secretAnswer
-      //   codeReturn.style.color = 'rgb(0,255,0)'
-      // } else {
-      //   codeReturn.textContent = 'This code is invalid'
-      //   codeReturn.style.color = 'rgb(255,0,0)'
-      // }
+      secretAnswer = await runQuery(inputValue).catch(console.dir)
+      setIsValidating(false)
+      setIsDisabled(false)
+      if (secretAnswer) {
+        console.log('secret answer: ', secretAnswer)
+        codeReturn.textContent = 'Validated! Secret answer is: ' + secretAnswer
+        codeReturn.style.color = 'rgb(0,255,0)'
+      } else {
+        codeReturn.textContent = 'This code is invalid'
+        codeReturn.style.color = 'rgb(255,0,0)'
+      }
     }
   }
 
